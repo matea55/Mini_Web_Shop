30897575c3dbc3745b6cf26f8f1a7e40323b8ee5
Matea Ilisevic <ilisevicmatea@gmail.com>
2022-07-22 11:08:17 +0200

    connection to database added

--- MiniWebShopApp/MiniWebShop/Properties/Settings.Designer.cs
+++ MiniWebShopApp/MiniWebShop/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MiniWebShop.Properties
-{
-
-
+namespace MiniWebShop.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=pterodactyl.next-cloud.ml\\mssql,1433;Initial Catalog=WebShop;Persist " +
+            "Security Info=True;User ID=UName;Password=Userpass#1")]
+        public string WebShopConnectionString {
+            get {
+                return ((string)(this["WebShopConnectionString"]));
             }
         }
     }
