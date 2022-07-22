209f5abd85f0da9f75193d599bac753ac0fa5c35
Matea Ilisevic <ilisevicmatea@gmail.com>
2022-07-22 12:24:52 +0200

    trying MVC approach, added product view in InitialView

---
+++ MiniWebShopApp/MiniWebShop/Controllers/GetProducts.cs
@@ -0,0 +1,22 @@
+﻿using MiniWebShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniWebShop
+{
+    public static class GetProducts
+    {
+        public static List<Proizvod> Products()
+        {
+           List<Proizvod> product;
+            using(var db = new WebShopModel())
+            {
+                product = db.Proizvods.ToList();
+            }
+            return product;
+        }
+    }
+}
